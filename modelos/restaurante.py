dd5e58f4f247fc5a896d28270c104cfb0c61868d
Your Name <jabes.candido1@gmail.com>
2024-08-06 20:01:00 -0300

    Implementando o @property

--- modelos/restaurante.py
+++ modelos/restaurante.py
@@ -4,15 +4,21 @@ class Restaurante:
     def __init__ (self, nome, categoria):
         self.nome = nome
         self.categoria = categoria
-        self.ativo = False
+        self._ativo = False # O undeline serve para proteger e não privar o acesso das pessoas de modificar a ativação do restaurante
         Restaurante.restaurantes.append(self)
     
     def __str__ (self):
         return f'{self.nome} | {self.categoria} | {self.ativo}'
     
     def listar_restaurantes():
+        print(f"{'Nome do Restaurante'.ljust(31)} | {'Categoria'.ljust(36)} | {'Status'}")
         for restaurante in Restaurante.restaurantes:
-            print(f"Nome: {restaurante.nome} | Categoria: {restaurante.categoria} | Ativo: {restaurante.ativo}")
+            print(f"Nome: {restaurante.nome.ljust(25)} | Categoria: {restaurante.categoria.ljust(25)} | Ativo: {restaurante.ativo.ljust(25)}")
+
+    @property #Modificar a forma como aquele atributo vai ser lido
+    def ativo(self):
+        return 'verdadeiro' if self._ativo else 'false'
+
 
 restaurante_praca = Restaurante('Praça', 'Gourmet')
 restaurante_pizza = Restaurante('Pazzaria express', 'Italiana')
