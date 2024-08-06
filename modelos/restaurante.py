475f163f31661d7cad00a02fd725b028dbe89879
Your Name <jabes.candido1@gmail.com>
2024-08-06 11:37:16 -0300

    Criando restaurantes

--- modelos/restaurante.py
+++ modelos/restaurante.py
@@ -2,3 +2,7 @@ class Restaurante:
     nome = ''
     categoria = ''
     ativo = False
+
+restaurante_praca = Restaurante()
+restaurante_pizza = Restaurante()
+restaurantes = [restaurante_praca, restaurante_pizza]
